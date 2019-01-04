10d446ac5d7a6fa88f53fcb65f896040b838018e
gdm <gdm.mbp@gdmMBP.local>
2019-01-04 18:25:26 -0500

    Add FaceID/TouchID authentication

--- SecureNote/Controller/NoteVC.swift
+++ SecureNote/Controller/NoteVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class NoteVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
@@ -41,13 +42,61 @@ class NoteVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        pushNoteFor(IndexPath: indexPath)    }
+        if notesArray[indexPath.row].lockStatus == .locked {
+            authenticateBiometrics(completion: { (authenticated) in
+                if authenticated {
+                    let lockStatus = notesArray[indexPath.row].lockStatus
+                    notesArray[indexPath.row].lockStatus = lockStatusFlipper(lockStatus)
+                    DispatchQueue.main.async {
+                        self.pushNoteFor(IndexPath: indexPath)
+
+                    }
+                }
+            })
+        } else {
+            pushNoteFor(IndexPath: indexPath)
+        }
+    }
     
     func pushNoteFor(IndexPath: IndexPath) {
         guard let noteDetailVC = storyboard?.instantiateViewController(withIdentifier: "NoteDetailVC") as? NoteDetailVC else { return }
         noteDetailVC.note = notesArray[IndexPath.row]
         noteDetailVC.index = IndexPath.row
         navigationController?.pushViewController(noteDetailVC, animated: true)
+    }
+    
+    func authenticateBiometrics(completion: @escaping (Bool) -> Void) {
+        let myContext = LAContext()
+        let myLocalizedReasonString = "This app uses FaceID to lock your notes."
+        var authError: NSError?
+        
+        if #available(iOS 8.0, macOS 10.12.1, *) {
+            if myContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &authError) {
+                myContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: myLocalizedReasonString, reply: { (success, evaluateError) in
+                    if success {
+                        completion(true)
+                    } else {
+                        guard let evaluateErrorString = evaluateError?.localizedDescription else { return }
+                        self.showAlert(withMessage: evaluateErrorString)
+                        completion(false)
+                    }
+                })
+            } else {
+                guard let authErrorString = authError?.localizedDescription else { return }
+                showAlert(withMessage: authErrorString)
+                completion(false)
+            }
+        } else {
+            completion(false)
+        }
+        
+    }
+    
+    func showAlert(withMessage message: String) {
+        let alertVC = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertVC.addAction(action)
+        present(alertVC, animated: true, completion: nil)
     }
 }
 
